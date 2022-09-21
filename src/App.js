d3a6fdd566c4b6a6526383e3fd59aef20d62124e
MisatoEve <eve.andrada3@gmail.com>
2022-09-21 01:13:04 -0300

    avances de app

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import AvatarJsx from './components/AvatarJsx';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">*/}
         <img src={logo} className="App-logo" alt="logo" />
         <AvatarJsx />
-        <h1>MisatoEve ^^</h1>      
+        <h1 className='Titulo' style={{ backgroundColor: 'green'}} onClick={() => console.log('click')}>MisatoEve ^^</h1>      
       <button>boton</button> 
-      <input></input>       
-      </header>
+      <input />       
+      {/* </header>*/}
     </div>
   );
 }
